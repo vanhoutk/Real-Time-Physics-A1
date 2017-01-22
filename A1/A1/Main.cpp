68041bc87b5f400f6b7b6c6384ae2fd0f658f06e
Kris <vanhoutk@tcd.ie>
2017-01-22 23:04:18 +0000

    Snow working pretty well. Need better wall/ground textures and possibly a nice skybox

--- A1/A1/Main.cpp
+++ A1/A1/Main.cpp
@@ -26,9 +26,9 @@
  */
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))  // Macro for indexing vertex buffer
 
-#define NUM_MESHES   1
-#define NUM_SHADERS	 2
-#define NUM_TEXTURES 1
+#define NUM_MESHES   2
+#define NUM_SHADERS	 3
+#define NUM_TEXTURES 3
 
 using namespace std;
 
@@ -44,31 +44,36 @@ bool firstMouse = true;
 bool keys[1024];
 Camera camera(vec3(0.0f, 0.0f, 3.0f));
 DWORD lastTime = 0;
-enum Shaders { SKYBOX, SPHERE };
+enum Meshes { PARTICLE_MESH, PLANE_MESH };
+enum Shaders { SKYBOX, PARTICLE_SHADER, BASIC_TEXTURE_SHADER };
 GLfloat cameraSpeed = 0.005f;
 GLfloat deltaTime = 1.0f / 60.0f;
-GLfloat friction = 0.1f;
+GLfloat friction = 0.3f;
 GLfloat lastX = 400, lastY = 300;
-GLfloat resilience = 0.1f;
+GLfloat resilience = 0.01f;
+GLfloat snowGravityFactor = 0.1f;
 GLuint cubemapTexture;
+GLuint groundVAO, groundTextureID;
 GLuint lastUsedParticle = 0;
-GLuint numParticles = 5000;
-GLuint numNewParticles = numParticles/(7*60);
+GLuint numParticles = 30000;
+GLuint numNewParticles = numParticles/(5*60);
 GLuint shaderProgramID[NUM_SHADERS];
 GLuint skyboxVAO, skyboxVBO;
-GLuint sphereTextureID, sphereVAO;
+GLuint particleTextureID, particleVAO;
+GLuint wallTextureID;
 int screenWidth = 1000;
 int screenHeight = 800;
-Mesh skybox;
-Mesh sphere;
+Mesh groundMesh;
+Mesh skyboxMesh;
+Mesh particleMesh;
 vec3 gravity = vec3(0.0f, -9.81f, 0.0f);
 vector<Particle> particles;
 
 // | Resource Locations
-const char * meshFiles[NUM_MESHES] = { "../Meshes/sphere2.dae" };
-const char * textureFiles[NUM_TEXTURES] = { "../Textures/particle.png" };
-const char * vertexShaderNames[] = { "../Shaders/SkyboxVertexShader.txt", "../Shaders/ParticleVertexShader.txt"};
-const char * fragmentShaderNames[] = { "../Shaders/SkyboxFragmentShader.txt", "../Shaders/ParticleFragmentShader.txt" };
+const char * meshFiles[NUM_MESHES] = { "../Meshes/particle.dae", "../Meshes/plane.dae" };
+const char * textureFiles[NUM_TEXTURES] = { "../Textures/particle.png", "../Textures/asphalt.jpg", "../Textures/building.jpg" };
+const char * vertexShaderNames[NUM_SHADERS] = { "../Shaders/SkyboxVertexShader.txt", "../Shaders/ParticleVertexShader.txt", "../Shaders/BasicTextureVertexShader.txt" };
+const char * fragmentShaderNames[NUM_SHADERS] = { "../Shaders/SkyboxFragmentShader.txt", "../Shaders/ParticleFragmentShader.txt", "../Shaders/BasicTextureFragmentShader.txt" };
 
 void display() 
 {
@@ -79,7 +84,7 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Draw skybox first
-	mat4 view = camera.GetViewMatrix(); // TODO: Figure out how to remove any translation component of the view matrix
+	mat4 view = camera.GetViewMatrix(); 
 	mat4 projection = perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
 
 	/*glUseProgram(shaderProgramID[SKYBOX]);
@@ -94,10 +99,41 @@ void display()
 	glDepthMask(GL_TRUE);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);*/
 
-	glUseProgram(shaderProgramID[SPHERE]);
-	glBindVertexArray(sphereVAO);
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[SPHERE], "view"), 1, GL_FALSE, view.m);
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[SPHERE], "proj"), 1, GL_FALSE, projection.m);
+
+
+	mat4 groundModel = identity_mat4();
+	groundModel = scale(groundModel, vec3(2.0f, 1.0f, 20.0f));
+	groundModel = translate(groundModel, vec3(0.0f, -1.0f, -5.0f));
+	vec4 groundColour = vec4(0.5f, 1.0f, 1.0f, 1.0f);
+
+
+	glBindVertexArray(groundVAO);
+	glUseProgram(shaderProgramID[BASIC_TEXTURE_SHADER]);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "view"), 1, GL_FALSE, view.m);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "projection"), 1, GL_FALSE, projection.m);
+
+	glBindTexture(GL_TEXTURE_2D, groundTextureID);
+	glUniform1i(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "basic_texture"), 0);
+	//glUniform4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "colour"), 1, groundColour.v);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "model"), 1, GL_FALSE, groundModel.m);
+	glDrawArrays(GL_TRIANGLES, 0, groundMesh.vertex_count);
+
+	mat4 wallModel = identity_mat4();
+	wallModel = scale(wallModel, vec3(2.0f, 1.0f, 20.0f));
+	wallModel = rotate_z_deg(wallModel, 90.0f);
+	mat4 leftWallModel = translate(wallModel, vec3(-1.0f, 0.0f, -5.0f));
+	mat4 rightWallModel = translate(wallModel, vec3(1.0f, 0.0f, -5.0f));
+	glBindTexture(GL_TEXTURE_2D, wallTextureID);
+	glUniform1i(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "basic_texture"), 0);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "model"), 1, GL_FALSE, leftWallModel.m);
+	glDrawArrays(GL_TRIANGLES, 0, groundMesh.vertex_count);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[BASIC_TEXTURE_SHADER], "model"), 1, GL_FALSE, rightWallModel.m);
+	glDrawArrays(GL_TRIANGLES, 0, groundMesh.vertex_count);
+	
+	glUseProgram(shaderProgramID[PARTICLE_SHADER]);
+	glBindVertexArray(particleVAO);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[PARTICLE_SHADER], "view"), 1, GL_FALSE, view.m);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[PARTICLE_SHADER], "projection"), 1, GL_FALSE, projection.m);
 	
 	for (Particle particle : particles)
 	{
@@ -106,9 +142,9 @@ void display()
 			mat4 model = identity_mat4();
 			model = scale(model, vec3(0.005f, 0.005f, 0.005f));
 			model = translate(model, particle.position);
-			glUniform4fv(glGetUniformLocation(shaderProgramID[SPHERE], "colour"), 1, particle.colour.v);
-			glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[SPHERE], "model"), 1, GL_FALSE, model.m);
-			glDrawArrays(GL_TRIANGLES, 0, sphere.vertex_count);
+			glUniform4fv(glGetUniformLocation(shaderProgramID[PARTICLE_SHADER], "colour"), 1, particle.colour.v);
+			glUniformMatrix4fv(glGetUniformLocation(shaderProgramID[PARTICLE_SHADER], "model"), 1, GL_FALSE, model.m);
+			glDrawArrays(GL_TRIANGLES, 0, particleMesh.vertex_count);
 		}
 	}
 	
@@ -151,7 +187,7 @@ void processForces()
 			
 			
 			// Apply the winds
-			vec3 upper_wind = vec3(0.01f, 0.0f, 0.0f);
+			vec3 upper_wind = vec3(0.015f, 0.0f, 0.0f);
 			//vec3 lower_wind = vec3(-0.05f, 0.0f, 0.0f);
 			//vec3 circular_wind = cross(particle.position, normal) * 0.001;
 			//circular_wind.v[1] = 0.0f;
@@ -175,7 +211,7 @@ void processForces()
 
 			// Apply Gravity Force
 			//particle.force += vec3(0.0f, -0.981f, 0.0f) * particle.mass;
-			particle.force += gravity * particle.mass;
+			particle.force += gravity * snowGravityFactor * particle.mass;
 			// Ideally need to calculate air resitance
 
 			// Calculate the velocity
@@ -232,11 +268,11 @@ GLuint FirstUnusedParticle()
 void RespawnParticle(Particle &particle)
 {
 	//GLfloat randomX = ((rand() % 100) - 50) / 1000.0f; // Fountain
-	GLfloat randomX = ((rand() % 100) - 50) / 75.0f; // Snow
+	GLfloat randomX = ((rand() % 1000) - 500) / 500.0f; // Snow
 	//GLfloat randomY = ((rand() % 100) - 50) / 0.75f;
 	GLfloat randomY = 1.5f;
 	//GLfloat randomZ = ((rand() % 100) - 50) / 1000.0f; // Fountain
-	GLfloat randomZ = ((rand() % 100) - 50) / 75.0f; // Snow
+	GLfloat randomZ = ((rand() % 1000) - 750) / 50.0f; // Snow
 	GLfloat randomRed = 0.0 + ((rand() % 15) / 100.0f);
 	GLfloat randomGreen = 0.3 + ((rand() % 25) / 100.0f);
 	GLfloat randomBlue = 0.5 + ((rand() % 50) / 100.0f);
@@ -244,7 +280,7 @@ void RespawnParticle(Particle &particle)
 	particle.position = vec3(randomX, randomY, randomZ);
 	//particle.colour = vec4(randomRed, randomGreen, 1.0f, 1.0f);
 	particle.colour = white;
-	particle.life = 7.0f;
+	particle.life = 5.0f;
 	particle.mass = 0.05f; // Snow
 	particle.velocity = vec3(0.0f, 0.0f, 0.0f);
 }
@@ -265,7 +301,7 @@ void updateParticles()
 		if (p.life > 0.0f)
 		{	// particle is alive, thus update
 			//p.position += p.velocity * deltaTime;
-			p.colour.v[3] -= deltaTime * 2.5;
+			p.colour.v[3] -= deltaTime/5.0f;
 		}
 	}
 }
@@ -287,10 +323,15 @@ void init()
 		shaderProgramID[i] = CompileShaders(vertexShaderNames[i], fragmentShaderNames[i]);
 	}
 	//CompileShaders(NUM_SHADERS, shaderProgramID, vertexShaderNames, fragmentShaderNames);
-	skybox.setupSkybox(&skyboxVAO, &skyboxVBO, &cubemapTexture);
-	sphere = Mesh(&shaderProgramID[SPHERE]);
-	sphere.generateObjectBufferMesh(sphereVAO, meshFiles[0]);
-	//sphere.loadTexture(textureFiles[0], &sphereTextureID);
+	//skyboxMesh.setupSkybox(&skyboxVAO, &skyboxVBO, &cubemapTexture);
+	groundMesh = Mesh(&shaderProgramID[BASIC_TEXTURE_SHADER]);
+	groundMesh.generateObjectBufferMesh(groundVAO, meshFiles[PLANE_MESH]);
+	groundMesh.loadTexture(textureFiles[1], &groundTextureID);
+	groundMesh.loadTexture(textureFiles[2], &wallTextureID);
+
+	particleMesh = Mesh(&shaderProgramID[PARTICLE_SHADER]);
+	particleMesh.generateObjectBufferMesh(particleVAO, meshFiles[PARTICLE_MESH]);
+	//particleMesh.loadTexture(textureFiles[0], &particleTextureID);
 
 	for (GLuint i = 0; i < numParticles; i++)
 		particles.push_back(Particle());
